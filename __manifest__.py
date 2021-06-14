5a1ff38e94d9a87d1daf1176af0b515aa6d39816
brahim94 <brahim-ayad@hotmail.com>
2021-06-14 06:38:35 -0700

    qr_code

--- __manifest__.py
+++ __manifest__.py
@@ -13,15 +13,15 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock'],
+    'depends': ['base', 'stock', 'product'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'report/stock_picking_ticket.xml',
+        'report/stock_picking_ticket.xml',
         'views/tickets.xml',
     ],
     # only loaded in demonstration mode
